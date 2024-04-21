171ce51ac906b71c12a9642c7af87527ad61cffe
daniescobarx <daniela.mollo@sptech.school>
2024-04-21 00:47:24 -0300

    add

---
+++ src/main/java/com/application/rest/controllers/MakerController.java
@@ -0,0 +1,38 @@
+package com.application.rest.controllers;
+
+import com.application.rest.controllers.DTO.MakerDTO;
+import com.application.rest.entities.Maker;
+import com.application.rest.service.IMakerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/maker")
+public class MakerController {
+
+    @Autowired
+    private IMakerService makerService;
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<?> findById(@PathVariable long id){
+        Optional<Maker> makerOptional = makerService.findById(id);
+
+        if (makerOptional.isPresent()){
+            Maker maker = makerOptional.get();
+
+            MakerDTO makerDTO = MakerDTO.builder()
+                    .id(maker.getId())
+                    .name(maker.getName())
+                    .productList(maker.getProductList())
+                    .build();
+        return ResponseEntity.ok(makerDTO);
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
