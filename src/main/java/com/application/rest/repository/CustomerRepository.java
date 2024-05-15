97ff47cf25cd6f7ed56b5e550dcc4e78d7feb6fb
daniescobarx <daniela.mollo@sptech.school>
2024-05-14 21:38:09 -0300

    ajuste erro de import

--- src/main/java/com/application/rest/repository/CustomerRepository.java
+++ src/main/java/com/application/rest/repository/CustomerRepository.java
@@ -1,8 +1,9 @@
 package com.application.rest.repository;
 
+import com.application.rest.entities.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<CustomerRepository, Long> {
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
 }
