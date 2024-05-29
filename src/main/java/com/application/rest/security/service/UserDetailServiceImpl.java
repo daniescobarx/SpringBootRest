d6c5ebe8449e895cc1e957c353c0f94807854267
daniescobarx <daniela.mollo@sptech.school>
2024-05-29 11:22:53 -0300

    add userDetailServiceImpl

---
+++ src/main/java/com/application/rest/security/service/UserDetailServiceImpl.java
@@ -0,0 +1,47 @@
+package com.application.rest.security.service;
+
+import com.application.rest.security.persistence.entity.UserEntity;
+import com.application.rest.security.persistence.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userEntity = userRepository.findUserEntityByUsername(username)
+                .orElseThrow() -> new UsernameNotFoundException("O usuario "+username+ " não existe.");
+
+        List<SimpleGrantedAuthority> authorityList = new ArrayList<>();
+
+        userEntity.getRoles()
+                .forEach(role -> authorityList.add(new SimpleGrantedAuthority("ROLE_".concat(role.getRoleEnum().name()))));
+
+        userEntity.getRoles().stream()
+                .flatMap(role -> role.getPermissionList().stream())
+                .forEach(permisson -> authorityList.add(new SimpleGrantedAuthority(permisson.getName())));
+
+        return new User(userEntity.getUsername(),
+                userEntity.getPassword(),
+                userEntity.isEnabled(),
+                userEntity.isAccountNoExpired(),
+                userEntity.isCredentialNoLocked(),
+                userEntity.isCredentialNoExpired(),
+                authorityList
+                );
+
+    }
+}
