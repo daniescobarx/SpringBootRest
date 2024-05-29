feeab647bd7cbdc7d0b26977b9089dad3c3839c6
daniescobarx <daniela.mollo@sptech.school>
2024-05-29 11:59:58 -0300

    add roles

--- src/main/java/com/application/rest/security/config/DataInitializer.java
+++ src/main/java/com/application/rest/security/config/DataInitializer.java
@@ -2,10 +2,13 @@ package com.application.rest.security.config;
 
 import com.application.rest.security.persistence.entity.PermissionEntity;
 import com.application.rest.security.persistence.entity.RoleEntity;
+import com.application.rest.security.persistence.entity.RoleEnum;
 import com.application.rest.security.persistence.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @Configuration
 public class DataInitializer {
@@ -28,7 +31,21 @@ public class DataInitializer {
                     .name("DELETE")
                     .build();
 
-            
+            //roles
+            RoleEntity roleAdmin = RoleEntity.builder()
+                    .roleEnum(RoleEnum.ADMIN)
+                    .permissionList(Set.of(createPemission, readPermission, updatePermission, deletePermission))
+                    .build();
+
+            RoleEntity roleMaker = RoleEntity.builder()
+                    .roleEnum(RoleEnum.MAKER)
+                    .permissionList(Set.of(createPemission))
+                    .build();
+
+            RoleEntity roleCustomer = RoleEntity.builder()
+                    .roleEnum(RoleEnum.CUSTOMER)
+                    .permissionList(Set.of(createPemission))
+                    .build();
 
         };
     }
